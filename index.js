183aeaaccb9ff491042e491c58628e7f39211452
pranab14 <96515289+pranab14@users.noreply.github.com>
2021-12-22 12:03:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// console.log("hello world");
+
+
+// var myname=("pranabquila");
+// console.log(myname);
+// var myAge="25";
+// console.log(myAge);
+// console.log(typeof('myAge'));
+// console.log(true + true );
+// var myName="78789";
+// console.log(isNaN(myName));
+// var x ="2";
+// var y ='2';
+// console.log( y == x);
+// console.log( "Sum = " + (2+2));
+// var newNo=10;
+// newNo--;
+// console.log("the value="+ newNo,+ result);
+// var result=newNo+5;
+// console.log("result"+result);
+// var today='rain';
+// debugger
+// if(today="rain")
+// {
+//     console.log("take a rain coate");
+// }
+// else{
+//     console.log("dont need to take rain coate")
+// }
+// While loop
+// var num=0;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+
+// do while loop
+
+// var num=0;
+// debugger;
+// do{
+//     console.log(num);
+//     num++
+// }
+// while(num<=10)
+
+// for(num=0;num<=10;num++)
+
+// {
+//     debugger;
+//     console.log(num);
+// }
+// for(num=1;num<=10;num++){
+//     var tabel=8;
+//     var result=tabel*num;
+//     console.log(result);
+
+// }
+
+
+// function 
+// function sum()
+// {
+//     var a=2;b=3;
+//     total=a+b;
+//     console.log(total);
+// }
+// sum();
+
+
+function sum(a,b){
+    total=a+b;
+    console.log(total);
+}
+sum(78,2);
+sum(78,20)
+sum();
+
